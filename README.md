f980c6e920535ab85c29ca288f6ba476d7af5e98
Winston Troughton <winston@troughton.net>
2022-08-12 21:10:33 -0400

    Initial darwin config

---
+++ README.md
@@ -0,0 +1,6 @@
+# nix-darwin
+
+Nix flake for darwin. This config controls system settings. For user settings, see [home-manager](https://github.com/wtroughton/home-manager).
+
+## Install
+Install [nix-darwin](https://github.com/LnL7/nix-darwin/blob/master/README.md#install). nix-darwin will need to be to be bootstrapped using the installer or manually. Afterwards the flake based configuration can be built. 
---
+++ configuration.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+{
+  system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
+
+  system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 12;  # 180ms
+  system.defaults.NSGlobalDomain.KeyRepeat = 2;  # 30ms
+
+  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+
+  system.defaults.NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = false;
+
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.dock.orientation = "left";
+  system.defaults.dock.showhidden = true;
+
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.FXEnableExtensionChangeWarning = false;
+
+  system.defaults.trackpad.Clicking = true;
+
+  time.timeZone = "America/New_York";
+
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    vim
+  ];
+
+  environment.variables.EDITOR = "nvim";
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # The user-friendly name for the system
+  networking.computerName = "HAL";
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
