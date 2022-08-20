b8d7cceb24fb360083b3cffa1d9c3dd412f71f73
Winston Troughton <winston@troughton.net>
2022-08-20 17:28:02 -0400

    Remove dotfiles and home configurations

--- darwin.nix
+++ darwin.nix
@@ -58,10 +58,6 @@
     })
   ];
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
--- flake.nix
+++ flake.nix
@@ -5,45 +5,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
     darwin.url  = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    emacs.url = "github:cmacrae/emacs";
-    emacs.inputs.nixpkgs.follows = "nixpkgs";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, darwin, home-manager, ... }@inputs: 
-    let 
-      overlays = with inputs; [
-        emacs.overlay
-        neovim-nightly-overlay.overlay
-      ];
-    in
-    {
-      darwinConfigurations = {
-        "Starlight" = darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
+  {
+    darwinConfigurations = {
+      "Starlight" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
 
-          modules = [
-            ./modules/darwin.nix
-            ./modules/pam.nix
-
-            home-manager.darwinModules.home-manager {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.winston = {
-                  imports = [ ./modules/home.nix ];
-                };
-              };
-            }
-
-            {
-              nixpkgs.overlays = overlays;
-            }
-          ];
-        };
+        modules = [
+          ./darwin.nix
+          ./modules/pam.nix
+        ];
       };
     };
+  };
 }
